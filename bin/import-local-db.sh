9bccc7930d60a235f6e97165cdcd2492f51150b8
David Martinez <david.martinez@praxent.com>
2018-01-30 11:53:56 -0600

    Added script to import database to local mysql

---
+++ bin/import-local-db.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ "$(pwd | grep bin)" ]
+then
+	if [ ! -f ../database/latest.sql ]; then
+    	echo "DB Backup file not found."
+    	exit
+	fi
+	echo "Updating domain name from backup..."
+	sed -i.bak 's|https://dev.bluebirdbotanicals.com|http://bluebird.local|g' ../database/latest.sql
+	sed -i.bak 's|https://admin.bluebirdbotanicals.com|http://bluebird.local|g' ../database/latest.sql
+	sed -i.bak 's|https://www.bluebird-botanicals.com|http://bluebird.local|g' ../database/latest.sql
+	sed -i.bak 's|https://bluebird-botanicals.com|http://bluebird.local|g' ../database/latest.sql
+	sed -i.bak 's|https://bluebirdbotanicals.com|http://bluebird.local|g' ../database/latest.sql
+	sed -i.bak 's|https://bluebirdbotanicals.com|http://bluebird.local|g' ../database/latest.sql
+	sed -i.bak 's|https://shop.bluebirdbotanicals.com|http://bluebird.local|g' ../database/latest.sql
+	sleep 1
+	echo "---------------------------------------"
+	echo "Restoring database from backup..."
+	mysql -u root -pelmismo --host=127.0.0.1 bluebird < ../database/latest.sql
+else
+	if [ ! -f database/latest.sql ]; then
+    	echo "DB Backup file not found."
+    	exit
+	fi
+	echo "Updating domain name from backup..."
+	sed -i.bak 's|https://dev.bluebirdbotanicals.com|http://bluebird.local|g' database/latest.sql
+	sed -i.bak 's|https://admin.bluebirdbotanicals.com|http://bluebird.local|g' database/latest.sql
+	sed -i.bak 's|https://www.bluebird-botanicals.com|http://bluebird.local|g' database/latest.sql
+	sed -i.bak 's|https://bluebird-botanicals.com|http://bluebird.local|g' database/latest.sql
+	sed -i.bak 's|https://bluebirdbotanicals.com|http://bluebird.local|g' database/latest.sql
+	sed -i.bak 's|https://shop.bluebirdbotanicals.com|http://bluebird.local|g' database/latest.sql
+	echo "---------------------------------------"
+	echo "Restoring database from backup..."
+	mysql -u root -pelmismo --host=127.0.0.1 bluebird < database/latest.sql
+fi
+
+echo "---------------------------------------"
+echo "done."
